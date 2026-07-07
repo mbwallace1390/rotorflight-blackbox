0eca827d1b7374fefecaa384f81634d655a27a5b
Michael Wallace <mbwallace1390@gmail.com>
2026-07-06 21:45:08 -0500

    Add Android application module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,77 @@
+import org.gradle.api.GradleException
+import org.gradle.api.tasks.Sync
+
+plugins {
+    id("com.android.application")
+}
+
+val webRoot = rootProject.projectDir.parentFile
+val generatedWebAssets = layout.buildDirectory.dir("generated/webAssets")
+
+val syncWebAssets by tasks.registering(Sync::class) {
+    from(webRoot) {
+        include("index.html")
+        include("index.js")
+        include("changelog.html")
+        include("manifest.json")
+        include("css/**")
+        include("images/**")
+        include("js/**")
+        include("locales/**")
+        include("_locales/**")
+        include("resources/**")
+        include("node_modules/bootstrap/**")
+        include("node_modules/html2canvas/**")
+        include("node_modules/lodash/**")
+        include("node_modules/webm-writer/**")
+        exclude("android/**")
+    }
+
+    into(generatedWebAssets)
+
+    doFirst {
+        val bootstrapCss = webRoot.resolve("node_modules/bootstrap/dist/css/bootstrap.min.css")
+        if (!bootstrapCss.isFile) {
+            throw GradleException(
+                "Blackbox web dependencies are missing. Run 'yarn install' or 'npm install' " +
+                    "in the repository root before building Android."
+            )
+        }
+    }
+}
+
+android {
+    namespace = "org.rotorflight.blackbox"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "org.rotorflight.blackbox"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    sourceSets {
+        getByName("main") {
+            assets.srcDir(generatedWebAssets)
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+tasks.named("preBuild").configure {
+    dependsOn(syncWebAssets)
+}
+
+dependencies {
+    implementation("androidx.webkit:webkit:1.16.0")
+}
