20bad5783bc9c0e621d123583903a9b280f70e22
Michael Wallace <mbwallace1390@gmail.com>
2026-07-06 21:48:17 -0500

    Allow localization fallback outside NW.js

--- js/localization.js
+++ js/localization.js
@@ -2,38 +2,61 @@
 
 function localize() {
     var localized = 0;
+    var i18n = window.chrome && window.chrome.i18n;
 
     var translate = function(messageID) {
-        localized++;
+        if (!i18n || typeof i18n.getMessage !== 'function') {
+            return null;
+        }
 
-        return chrome.i18n.getMessage(messageID);
+        var message = i18n.getMessage(messageID);
+        if (message) {
+            localized++;
+            return message;
+        }
+
+        return null;
     };
 
     $('[i18n]:not(.i18n-replaced)').each(function() {
         var element = $(this);
+        var message = translate(element.attr('i18n'));
 
-        element.html(translate(element.attr('i18n')));
+        // The source HTML already contains English labels. Keep that text when
+        // Chrome/NW.js localization APIs are unavailable, such as Android WebView.
+        if (message !== null) {
+            element.html(message);
+        }
         element.addClass('i18n-replaced');
     });
 
     $('[i18n_title]:not(.i18n_title-replaced)').each(function() {
         var element = $(this);
+        var message = translate(element.attr('i18n_title'));
 
-        element.attr('title', translate(element.attr('i18n_title')));
+        if (message !== null) {
+            element.attr('title', message);
+        }
         element.addClass('i18n_title-replaced');
     });
 
     $('[i18n_value]:not(.i18n_value-replaced)').each(function() {
         var element = $(this);
+        var message = translate(element.attr('i18n_value'));
 
-        element.val(translate(element.attr('i18n_value')));
+        if (message !== null) {
+            element.val(message);
+        }
         element.addClass('i18n_value-replaced');
     });
 
     $('[i18n_placeholder]:not(.i18n_placeholder-replaced)').each(function() {
         var element = $(this);
+        var message = translate(element.attr('i18n_placeholder'));
 
-        element.attr('placeholder', translate(element.attr('i18n_placeholder')));
+        if (message !== null) {
+            element.attr('placeholder', message);
+        }
         element.addClass('i18n_placeholder-replaced');
     });
 
